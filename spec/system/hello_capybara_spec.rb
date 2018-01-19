23b98866ab25536bf60793016c1b8ba3758c7172
AnnHryt <hrytsyuka@gmail.com>
2018-01-19 14:25:50 -0800

    Check failing test on travis ci

--- spec/system/hello_capybara_spec.rb
+++ spec/system/hello_capybara_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe "Hello Capybara" do
 	
   it "Should see Hello Capybara text on the page", type: :system, js: true do
     visit root_path
-    expect(page).to have_text("Hello Capybara!")
+    expect(page).to have_text("Hello Rails!")
   end
 end
