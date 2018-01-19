32996b069b0f6348893e301bef0d2e6caa7a220c
hrytsyuka <anastasiyah@amco.me>
2018-01-19 14:53:23 -0800

    basic test should pass

--- spec/system/hello_capybara_spec.rb
+++ spec/system/hello_capybara_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe "Hello Capybara" do
 	
   it "Should see Hello Capybara text on the page in Chrome browser", type: :system, js: true do
     visit root_path
-    expect(page).to have_text("Hello Rails!")
+    expect(page).to have_text("Hello Capybara!")
   end
 
   it "Should see Hello Capybara text on the page in Headless Chrome", type: :system do
     visit root_path
-    expect(page).to have_text("Hello Rails!")
+    expect(page).to have_text("Hello Capybara!")
   end
 end
